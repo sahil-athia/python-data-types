c18b115dd2bf208e699aa74176010cd251af16e9
Sahil Athia <sahilathia918@gmail.com>
2021-02-15 16:11:58 -0500

    basic python arithmatic

---
+++ numbers.py
@@ -0,0 +1,10 @@
+print(2 + 1, 3 - 4, 7 * 9, 9 / 3, 7 % 4, 2 ** 3) 
+# % sign is the mod operator, diplays remainder
+# ** is to the power
+
+print(2 + 10 * 10 + 3)
+print((2 + 10) * (10 + 3))
+# print 1 will use basic order of operations (* --> +)
+
+print(0.2 + 0.1 - 0.3)
+# this will not result in 0 since we are using folating point numbers
