7f889ddc5469a43fe2b23fb9e94224a68e38694d
Sahil Athia <sahilathia918@gmail.com>
2021-02-16 17:45:19 -0500

    python booleans

---
+++ boolean.py
@@ -0,0 +1,11 @@
+# true and false must be capitalized to work 
+bool1 = True
+bool2 = False
+
+print bool1
+print type(bool2)
+
+# if we dont want to set something we can use None
+
+nothing = None
+print type(nothing)
