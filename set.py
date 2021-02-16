94307548286787313834819157e97894ecbffaa2
Sahil Athia <sahilathia918@gmail.com>
2021-02-16 17:39:58 -0500

    basic use of set

---
+++ set.py
@@ -0,0 +1,13 @@
+# sets are a collection of unique elements
+
+my_set = set()
+print my_set
+
+my_set.add(1)
+print my_set
+
+my_set.add(2)
+print my_set
+
+my_list = [1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5]
+print set(my_list)
